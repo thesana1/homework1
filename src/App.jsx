b74462a9e22c8ba31906ad8752796f0eee034f09
thesana1 <giorgisanadiradze19@gmail.com>
2025-05-10 22:03:22 +0400

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  let name = "giorgi"
-  let lastname = "sanadiradze"
+  let name = "Giorgi"
+  let lastname = "Sanadiradze"
   let age = 16
-  let hobby = "coding"
+  let hobby = "Coding & Gaming"
 
   return (
     <div className='x1'>
-      <h1>name:  {name}</h1>
-      <h1>lastname:  {lastname}</h1>
-      <h1>age:  {age}</h1>
-      <h1>hobby:  {hobby}</h1>
+      <h1>Name:  <span style={{fontWeight:"10"}}>{name}</span></h1>
+      <h1>Last Name:   <span style={{fontWeight:"10"}}>{lastname}</span></h1>
+      <h1>Age:   <span style={{fontWeight:"10"}}>{age}</span></h1>
+      <h1>Hobby:   <span style={{fontWeight:"10"}}>{hobby}</span></h1>
     </div>
   )
 }
